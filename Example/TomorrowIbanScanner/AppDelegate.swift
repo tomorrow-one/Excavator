0bb637acfd511c30292fcc84bb40fa8ee7dbba91
Pavel Stepanov <>
2020-05-27 12:28:08 +0200

    Added swiftlint. Fixed warnings and some TODOs

--- Example/TomorrowIbanScanner/AppDelegate.swift
+++ Example/TomorrowIbanScanner/AppDelegate.swift
@@ -9,10 +9,9 @@
 import UIKit
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
+final class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
@@ -43,4 +42,3 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
 }
-
---
+++ TomorrowIbanScanner/Classes/TextRecognizer/TextRecognizerConfig.swift
@@ -0,0 +1,8 @@
+//
+//  TextRecognizerConfig.swift
+//  Nimble
+//
+//  Created by Pavel Stepanov on 27.05.20.
+//
+
+import Foundation
